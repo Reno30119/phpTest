2c1a6a5b315c90efd64e6c88fbb602fcfa4a70b6
Reno30119 <39398630+Reno30119@users.noreply.github.com>
2024-02-19 12:03:43 +0800

    Update phpTest.php

--- phpTest.php
+++ phpTest.php
@@ -1,3 +1,27 @@
-<?php
-    echo "This is a massage from website!"
+<?php 
+    $title = "Reno的第一個網站";
+    $bgColor = "#D5D794";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title?></title>
+    <style>
+        body{
+            background-color: <?php echo $bgColor; ?>;
+        }
+    </style>
+</head>
+<body>
+
+    <form method="post" action="test.php">
+        姓名 : <input type="text" name="name"/>
+        年齡 : <input type="text" name="age"/>
+        <input type="submit"/>
+    </form>
+    
+</body>
+</html>
